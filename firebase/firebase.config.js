44bafffababba56aeaf1e59f9ba87e80b8a01c50
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-10-05 18:11:55 +0700

    landing page dan firebase

---
+++ firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy9SoIVdX-1fZSGbdko_rOLvXbVwGHm0U",
+  authDomain: "server-binar-chapter-10.firebaseapp.com",
+  projectId: "server-binar-chapter-10",
+  storageBucket: "server-binar-chapter-10.appspot.com",
+  messagingSenderId: "210538881061",
+  appId: "1:210538881061:web:7c4d6a9c6c57197f399fcc"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
